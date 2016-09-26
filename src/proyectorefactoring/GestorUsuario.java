821db100f006f51ea0dbf479e3173cd521baf737
W5208 <W5208@lab.aca>
2016-09-26 09:25:08 -0500

    Refactor al metodo registrarUsuario

--- src/proyectorefactoring/GestorUsuario.java
+++ src/proyectorefactoring/GestorUsuario.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class GestorUsuario {
     
-    public void registrarUsuario(String nombre, String apellido, String nombreUsuario, long dni, Date fechaNac, String universidad, String contraseña, long telefono, String direccion, String distrito){
+    public void registrarUsuario(Usuario usuario){
         
         System.out.println("Se guardó el usuario");
         
--- src/proyectorefactoring/ProyectoRefactoring.java
+++ src/proyectorefactoring/ProyectoRefactoring.java
@@ -1,8 +1,29 @@
 package proyectorefactoring;
 
+import java.util.Date;
+
 public class ProyectoRefactoring {
 
     public static void main(String[] args) {
+        
+        String nombre = "Alejandro";
+        String apellido = "Arevalo";
+        String nombreUsuario = "Alemu";
+        long dni = 70480255;
+        Date fechaNac = new Date(1995, 8, 5);
+        String universidad = "Universidad de Lima";
+        String contraseña = "alemu$%&/()";
+        long telefono = 999565242;
+        String direccion = "Av. Javier Prado Oeste 105";
+        String distrito = "San Miguel";
+        
+        Usuario usuario1 = new Usuario(nombre, apellido, nombreUsuario, dni, fechaNac, universidad, contraseña, telefono, direccion, distrito);
+        
+        GestorUsuario generarUsuario = new GestorUsuario();
+        
+        generarUsuario.registrarUsuario(usuario1);
+        
+        
         
         
         
